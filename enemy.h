38022526656317379a4407194d5fafa4964b1ad1
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-05-10 23:31:44 +0200

    added cards to enemies
    i assume this is how slay the spire does it too. no need to give monsters their own attack system

--- enemy.h
+++ enemy.h
@@ -1,24 +1,27 @@
 #pragma once
+#include "cards.h"
 #include <string>
-#include <vector>
+#include <array>
 
 class enemy{
     std::string ch_type;
     int ch_id;
     int ch_health;
     int ch_mana;
+    std::array<card, 10> ch_enemyCards;
 public:
-    enemy(std::string type, int id, int health, int mana)
-        : ch_type(type), ch_id(id), ch_health(health), ch_mana(mana){}
+    enemy(std::string type, int id, int health, int mana, std::array<card, 10> enemyCards)
+        : ch_type(type), ch_id(id), ch_health(health), ch_mana(mana), ch_enemyCards(enemyCards){}
 };
 
 struct monsterTemplate{
     std::string name;
     std::string desc;
     int health;
-    int mana = 0;
+    int mana;
+    std::array<card, 10> ch_enemyCards;
 };
 
 monsterTemplate allMonsters[1] = {
-    {"Skeleton", "The skeleton appears", 20, 3}
+    {"Skeleton", "The skeleton appears", 20, 3, {common[1]}}
 };
