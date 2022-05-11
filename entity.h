10750f6ffb228ec669715d39980a85488d6a06d8
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-05-11 16:59:48 +0200

    added takeDamage to entity class

--- entity.h
+++ entity.h
@@ -5,8 +5,19 @@ class entity{
 protected:
     std::string ch_type;
     int ch_health;
+    int ch_maxHealth;
     int ch_mana;
+    int ch_block;
 public:
-    entity(std::string type, int health, int mana)
-        : ch_type(type), ch_health(health), ch_mana(mana){}
+    entity(std::string type, int health, int mana, int block = 0)
+        : ch_type(type), ch_health(health), ch_mana(mana), ch_block(block){ch_maxHealth = ch_health;}
+
+    void takeDamage(int dmg, bool ignoreBlock = false){
+        if (ignoreBlock){
+            ch_health -= dmg;
+        } else {
+            int totalDamage = dmg - ch_block;
+            ch_health -= (totalDamage < 1) ? 0 : totalDamage;
+        }
+    }
 };
