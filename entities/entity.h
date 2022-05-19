1ab5558c716274cef5f79d61f42f522e41e34af8
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-05-19 21:45:42 +0200

    added block funcs to entity class
    i have no idea what im doing or if this is even a good way of going about it

--- entities/entity.h
+++ entities/entity.h
@@ -14,11 +14,15 @@ public:
     entity(std::string type, int health, int mana, int block, std::string team)
         : ch_type(type), ch_health(health), ch_mana(mana), ch_block(block), ch_team(team){ch_maxHealth = ch_health;}
 
+    void upBlock(int block){ch_block += block;}
+    void downBlock(int block){ch_block = ((ch_block - block) < 0) ? 0 : ch_block - block;}
+
     void takeDamage(int dmg, bool ignoreBlock = false){
         if (ignoreBlock){
             ch_health -= dmg;
         } else {
             int totalDamage = dmg - ch_block;
+            downBlock(dmg);
             ch_health -= (totalDamage < 1) ? 0 : totalDamage;
         }
     }
