0063e2785bbf6db224ad850ccc2f6349862458d6
Tycho Hartingsveld <99062718@mydavinci.nl>
2022-06-04 23:56:28 +0200

    added deconstructor for ch_combatants
    turns out pointers not assigned to anything dont automatically turn into nullptr. something new learned everyday!

--- battle.h
+++ battle.h
@@ -8,7 +8,8 @@ class battle{
 public:
     entity* ch_combatants[50];
 
-    battle(std::vector<monsterTemplate> enemies, std::vector<allyData> allies){
+    battle(std::vector<monsterTemplate> enemies, std::vector<allyData> allies)
+        : ch_combatants(){
         for (int loopNum = 0; loopNum < allies.size(); loopNum++){
             allyData currentAlly = allies[loopNum];
             ch_combatants[loopNum] = new ally(currentAlly);
@@ -18,5 +19,11 @@ public:
             monsterTemplate currentEnemy = enemies[loopNum];
             ch_combatants[loopNum + 25] = new enemy(currentEnemy);
         }
-    };
+    }
+
+    ~battle(){
+        for (entity* currentEntity : ch_combatants){
+            delete currentEntity;
+        }
+    }
 };
