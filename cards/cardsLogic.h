f825bc08c2022dfc0553c0b80999d1e7159a197d
Tycho Hartingsveld <70889437+99062718@users.noreply.github.com>
2022-06-08 09:52:49 +0200

    changed several ints to chars
    tiny improvements (i hope)

--- cards/cardsLogic.h
+++ cards/cardsLogic.h
@@ -4,22 +4,22 @@
 #include <string>
 #include <array>
 
-std::array<int, 2> getAOIsize(std::string AOIeffect, int id, std::array<int, 2> teamSize){
+std::array<char, 2> getAOIsize(std::string AOIeffect, char id, std::array<char, 2> teamSize){
     return {(AOIeffect == "all") ? teamSize[0] : id + teamSize[0], (AOIeffect == "single") ? id + teamSize[0] : teamSize[1]};
 }
 
-std::array<int, 2> getTargetTeam(int id, bool target){
+std::array<char, 2> getTargetTeam(char id, bool target){
     if (id >= 25) target = !(target);
     
     return {(target == 0) ? 0 : 25, (target == 0) ? 25 : 50};
 }
 
-void activateCard(int userId, card currentCard, int teamId, battle& currentBattle){
+void activateCard(char userId, card currentCard, char teamId, battle& currentBattle){
     for (cardEffect currentEffect : currentCard.effects){
-        std::array<int, 2> teamSize = getTargetTeam(userId, currentEffect.target);
-        std::array<int, 2> loopSize = getAOIsize(currentEffect.AOIeffect, teamId, teamSize);
+        std::array<char, 2> teamSize = getTargetTeam(userId, currentEffect.target);
+        std::array<char, 2> loopSize = getAOIsize(currentEffect.AOIeffect, teamId, teamSize);
 
-        for (int loopNum = loopSize[0]; loopNum < loopSize[1]; loopNum++){
+        for (char loopNum = loopSize[0]; loopNum < loopSize[1]; loopNum++){
             switch (currentEffect.type){
                 case 0: 
                     currentBattle.ch_combatants[loopNum]->takeDamage(currentEffect.value);
